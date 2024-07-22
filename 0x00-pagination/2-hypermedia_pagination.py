ee1a294beafc89c0349b1ced3a7749c62d974dd7
root <root@TasnimSomo>
2024-07-22 11:15:45 +0400

    0x00. Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -46,23 +46,33 @@ class Server:
         dataset = self.dataset()
         return dataset[start_index:end_index]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """returns a dictionary containing the following key-value pairs
         """
-        Implement a get_hyper method that takes the same arguments (and defaults) as get_page and returns a dictionary containing the following key-value pairs:
-        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
         data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
 
-        total_items = len(self.dataset())
-        total_pages = math.ceil(total_items / page_size)
+        start, end = index_range(page, page_size)
 
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
+        # estimating the next page
+        if (page < total_pages):
+            next_page = page + 1
+        else:
+            next_page = None
 
-        return {
-            'page_size': len(data),
-            'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages
-        }
+        # estimating the previous page
+        if (page == 1):
+            prev_page = None
+        else:
+            prev_page = page - 1
+
+        return {'page_size': len(data),
+                'page': page,
+                'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': total_pages
+                }
